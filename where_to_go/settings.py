ddc5b5af80f8858e7bb1251daf5d354ea4118bd9
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2021-11-04 18:15:40 +0300

    setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
